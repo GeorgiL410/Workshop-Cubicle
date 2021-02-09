80defd2704c56e7b1e4e84f826fdb5743aa8d172
Georgi L <70279776+GeorgiL410@users.noreply.github.com>
2021-02-09 23:48:13 +0200

    fix services support for the home page

--- controllers/navigationController.js
+++ controllers/navigationController.js
@@ -8,33 +8,37 @@ const services = require('../services/services');
 // add the different paths and handlers
 router.get('/', (req, res) => {
 
-  let items = services.getAllPuzzles(req.query);
-  res.render('home', { title: 'Home', items });
-})
+  services.getAllPuzzles(req.query)
+    .then(products => {
+      console.log(products);
+      res.render('home', { title: 'Browse', products });
+    })
+    .catch(() => res.status(500).end());
+});
 
 router.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
-})
+});
 
 router.get('/create', (req, res) => {
   res.render('create', { title: 'Create' });
-})
+});
 
 router.post('/create', services.validate, (req, res) => {
   services.create(req.body);
 
   res.redirect('/');
 
-})
+});
 router.get('/details/:productId', (req, res) => {
   let id = req.params.productId;
   let puzzle = services.getDetails(id);
   res.render('details', { title: 'Product Details', puzzle });
 
-})
+});
 
 router.get('*', (req, res) => {
   res.render('404', { title: 'Not Found' });
-})
+});
 
 module.exports = router;
--- services/services.js
+++ services/services.js
@@ -1,17 +1,17 @@
 //The purpose of this file is to navigate the database and record/retrieve data where necessary
 
-const Model = require('../models/cube');
+const Cube = require('../models/cube');
 const Generic = require('../models/Model');
 
 
 const services = {
   create(data) {
-    let cube = new Model(data);
-    return cube.save();
-},
-  getAllPuzzles(searchParams) {
-    //get all items
-    let items = Generic.getAll();
+    let item = new Cube(data);
+    return item.save();
+  },
+  async getAllPuzzles(searchParams) {
+
+    let items = await Cube.find({});
     //filter items based on search inputs (name, difficulty from/to)
     if (searchParams.search) {
       items = items.filter((x) => x.name.includes(searchParams.search));
@@ -27,27 +27,27 @@ const services = {
     //return the filtered items so that they can be populated
     return items;
   },
-    getDetails(id) {
-  return Generic.getDetails(id);
-},
-validate(req, res, next) {
-  let isValid = false;
+  getDetails(id) {
+    return Cube.findById(id);
+  },
+  validate(req, res, next) {
+    let isValid = false;
 
-  if (req.body.name.trim().length > 2) {
-    isValid = true;
-  } else {
-    isValid = false
+    if (req.body.name.trim().length > 2) {
+      isValid = true;
+    } else {
+      isValid = false
+    }
+    if (req.body.imageUrl.trim().length > 5) {
+      isValid = true;
+    } else {
+      isValid = false
+    }
+    if (isValid) {
+      next();
+    } else {
+      res.redirect('/');
+    }
   }
-  if (req.body.imageUrl.trim().length > 5) {
-    isValid = true;
-  } else {
-    isValid = false
-  }
-  if (isValid) {
-    next();
-  } else {
-    res.redirect('/');
-  }
-}
 }
 module.exports = services;
